bb4c1d08050372095ae864d80ec7b68b03a35c8f
GarethCott <gareth.cott@gmail.com>
2023-06-27 21:02:39 +0200

    Add pages and basic routing

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,6 +1,7 @@
 import main from '../assets/images/main.svg'
 import Wrapper from '../assets/wrappers/LandingPage'
 import {Logo} from '../components'
+import {Link} from 'react-router-dom'
 
 const Landing = () => {
   return (
@@ -17,7 +18,7 @@ const Landing = () => {
           <p>
             Look for jobs as easy as creating a profile.
           </p>
-          <button className='btn btn-hero'>Login/Register</button>
+          <Link to='/register' className='btn btn-hero'>Login/Register</Link>
         </div>
         <img src={main} alt="job hunt" className='img main-img'/>
       </div>
